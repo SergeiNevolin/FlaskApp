89d8947b858753317569b2bf3eddf319dda70770
SergeiNevolin <sergeinevolin9@gmail.com>
2021-10-14 23:06:49 +0500

    first commit

---
+++ app/post_db_conn.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    return post
+
+def get_img(id):
+    return "/images/sample_img.jpg"
